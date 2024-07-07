13b8ceabe961c126fc769d679bdcc15c0d27c3f0
Ubuntu <ubuntu@ip-172-31-34-1.eu-north-1.compute.internal>
2024-07-07 17:19:12 +0000

    added new line

--- Helloword.java
+++ Helloword.java
@@ -3,7 +3,7 @@ class helloword{
     public static void main(String[] args){
         System.out.println("hello world!");
 	System.out.println("add new line");
-	System.out.println("add new line-afer adding new remote repo");    
+	System.out.println("add new line-afer adding new remote repo name");    
     }
     
 }
